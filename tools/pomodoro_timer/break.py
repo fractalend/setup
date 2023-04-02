41efb9c6ef4f81d26f746327f8fa07a71a79b33a
fractalend <fractalic.end@gmail.com>
2023-04-03 01:48:30 +0300

    add new folder for tools

---
+++ tools/pomodoro_timer/break.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import time
+import sys
+import os
+
+if(len(sys.argv) != 2 or not isinstance(int(sys.argv[1]), int)):
+    print("Usage: break <Number of Minutes>");
+    exit();
+
+alert = "gedit stop_msg"
+print("Start");
+minutes = int(sys.argv[1]);
+time.sleep(minutes*60);
+for i in range(3):
+    print("▄▄▄▄▄▄▄▄▄▄▄", end=" ", flush=True);
+    time.sleep(1);
+os.system(alert);
+
+
+
+
+
+
+
+
