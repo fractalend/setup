5e04be998833de7c6c8b26285abf0b1587440e58
z <z@z.z>
2024-11-09 20:15:55 +0300

    keep the notification visible until user close it

--- tools/pomodoro_timer/session.py
+++ tools/pomodoro_timer/session.py
@@ -7,7 +7,7 @@ if(len(sys.argv) != 2 or not isinstance(int(sys.argv[1]), int)):
     print("Usage: session.py <Number of Minutes>");
     exit();
 
-alert = "notify-send -a \"Session End\" STOP "
+alert = "notify-send -a  \"Session End\" -u critical \"STOP\""
 print(f'started at  {time.ctime()}');
 minutes = int(sys.argv[1]);
 time.sleep(minutes*60);
